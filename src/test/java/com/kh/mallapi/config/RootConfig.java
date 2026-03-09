cd0714e1dc867bdf31f0867483385541dc4fb00d
KTH <sauce0428@gmail.com>
2026-03-09 17:28:20 +0900

    루트콘피그 생성

--- src/test/java/com/kh/mallapi/config/RootConfig.java
+++ src/test/java/com/kh/mallapi/config/RootConfig.java
@@ -10,9 +10,9 @@ public class RootConfig {
 	@Bean
 	public ModelMapper getMapper() {
 		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setFieldMatchingEnabled(true)
-				.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-				.setMatchingStrategy(MatchingStrategies.LOOSE);
+		modelMapper.getConfiguration().setFieldMatchingEnabled(true) //이름이일치할경우 매핑
+				.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE) //필드접근수준 private
+				.setMatchingStrategy(MatchingStrategies.LOOSE); //필드이름이 비슷하면 매핑
 		return modelMapper;
 	}
 }
