dd991841fc2f97c932e394ea68ec661fe9299eda
KTH <sauce0428@gmail.com>
2026-03-09 17:28:51 +0900

    투두 서비스, 임플 생성

--- src/test/java/com/kh/mallapi/service/TodoService.java
+++ src/test/java/com/kh/mallapi/service/TodoService.java
@@ -3,5 +3,11 @@ package com.kh.mallapi.service;
 import com.kh.mallapi.dto.TodoDTO;
 
 public interface TodoService {
-	Long register(TodoDTO todoDTO); 
+	Long register(TodoDTO todoDTO);
+
+	void modify(TodoDTO todoDTO);
+
+	void remove(Long tno);
+	
+	TodoDTO get(Long tno);
 }
--- src/test/java/com/kh/mallapi/service/TodoServiceImpl.java
+++ src/test/java/com/kh/mallapi/service/TodoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kh.mallapi.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +19,11 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
 
-	//@Override
-	//public Long register(TodoDTO todoDTO) {
-	//	log.info(" ");
-	//	return null;
-	//}
+	// @Override
+	// public Long register(TodoDTO todoDTO) {
+	// log.info(" ");
+	// return null;
+	// }
 
 	// 자동주입 대상은 final로
 	private final ModelMapper modelMapper;
@@ -35,4 +37,27 @@ public class TodoServiceImpl implements TodoService {
 
 		return savedTodo.getTno();
 	}
+
+	@Override
+	public void modify(TodoDTO todoDTO) {
+		Optional<Todo> result = todoRepository.findById(todoDTO.getTno());
+		Todo todo = result.orElseThrow();
+		todo.changeTitle(todoDTO.getTitle());
+		todo.changeDueDate(todoDTO.getDueDate());
+		todo.changeComplete(todoDTO.isComplete());
+		todoRepository.save(todo);
+	}
+
+	@Override
+	public void remove(Long tno) {
+		todoRepository.deleteById(tno);
+	}
+
+	@Override
+	public TodoDTO get(Long tno) {
+		java.util.Optional<Todo> result = todoRepository.findById(tno);
+		Todo todo = result.orElseThrow();
+		TodoDTO dto = modelMapper.map(todo, TodoDTO.class);
+		return dto;
+	}	
 }
